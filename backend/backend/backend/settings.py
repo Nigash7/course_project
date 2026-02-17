ce214422d7ecb4d9e2b02f2e3d171d9db1b646ac
Nigash7 <nigash6385@gmail.com>
2026-02-17 10:11:45 +0530

    Added STATIC_ROOT for deployment

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
